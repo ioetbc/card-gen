add7b0ec56acba5e6b55358267495e8634cb7757
willtrystatement <ioetbc@gmail.com>
2023-08-28 21:29:16 +0100

    details layout

--- src/app/components/product-details.tsx
+++ src/app/components/product-details.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {BottomSheet} from "react-spring-bottom-sheet";
 import {TProduct} from "../types";
 import {PrimaryButton} from "./buttons/primary-button";
@@ -10,7 +10,7 @@ type ProductDetailsProps = {
   handleClose: () => void;
 };
 
-const ProductDisplay = () => {
+export const ProductDetails = ({product, handleClose}: ProductDetailsProps) => {
   const checkout = useCheckoutSession();
 
   useEffect(() => {
@@ -23,45 +23,6 @@ const ProductDisplay = () => {
     checkout.mutate();
   };
 
-  return (
-    <section>
-      <div className="product">
-        <div className="description">
-          <h3>Stubborn Attachments</h3>
-          <h5>$20.00</h5>
-        </div>
-      </div>
-      <form onSubmit={handleCheckout} method="POST">
-        <button type="submit">Checkout</button>
-      </form>
-    </section>
-  );
-};
-
-const Message = ({message}: {message: string}) => (
-  <section>
-    <p>{message}</p>
-  </section>
-);
-
-export const ProductDetails = ({product, handleClose}: ProductDetailsProps) => {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-
-    if (query.get("success")) {
-      setMessage("Order placed! You will receive an email confirmation.");
-    }
-
-    if (query.get("canceled")) {
-      setMessage(
-        "Order canceled -- continue to shop around and checkout when you're ready."
-      );
-    }
-  }, []);
-
   const description =
     "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
@@ -73,20 +34,19 @@ export const ProductDetails = ({product, handleClose}: ProductDetailsProps) => {
       onDismiss={handleClose}
       maxHeight={window?.innerHeight - 80}
     >
-      <div className="md:flex md:gap-4 p-8">
+      <div className="md:grid md:grid-cols-2 md:gap-4 p-8">
         <div className="flex mb-8 md:mb-0">
           {product?.url && (
-            <Image width={256} height={256} src={product.url} alt="card" />
+            <Image width={500} height={500} src={product.url} alt="card" />
           )}
         </div>
         <div className="flex flex-col gap-4 mb-16">
-          <h1 className="text-4xl md:text-center">{product?.title}</h1>
+          <h1 className="text-4xl">{product?.title}</h1>
           <p className="text-xl">{description}</p>
           <p className="text-xl">£{price}</p>
+          <PrimaryButton label="Purchase" handleOnClick={handleCheckout} />
         </div>
-        <PrimaryButton label="Purchase" handleOnClick={() => {}} />
       </div>
-      {message ? <Message message={message} /> : <ProductDisplay />}
     </BottomSheet>
   );
 };
